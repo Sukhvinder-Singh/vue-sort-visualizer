0314636cbb43dce17c2fe426fc383d533e088faa
Sukhvinder Singh <sukhvisukh@gmail.com>
2023-03-27 12:42:48 +0530

    added bootstrap in minor code refactoring

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
-  <button @click="bubbleSort(array)">Start</button>
+  <div class="settings">
+    <h1>Vue sort visualizer</h1>
+    <hr />
+    <button @click="bubbleSort(array)">Start</button>
+  </div>
   <div class="chart" ref="chartContainer">
     <span
       class="chart__bar"
       v-for="(val, index) in array"
       :key="`bar-${index}`"
       :style="{
-        width: `calc(${100 / array.length}% - 2px)`,
+        width: `calc(${100 / elementLength}% - 2px)`,
         height: `${(100 / elementLength) * val}%`,
-        left: `${(100 / array.length) * index}%`
+        left: `${(100 / elementLength) * index}%`
       }"
     ></span>
   </div>
@@ -18,7 +22,8 @@
 import { ref } from 'vue'
 
 const chartContainer = ref(null)
-const elementLength = 300
+const elementLength = 100
+const interval = 10
 
 const array = ref([])
 
@@ -27,8 +32,8 @@ for (let i = 0; i < elementLength; i++) {
 }
 
 for (let i = array.value.length - 1; i > 0; i--) {
-  const j = Math.floor(Math.random() * (i + 1));
-  [array.value[i], array.value[j]] = [array.value[j], array.value[i]]
+  const j = Math.floor(Math.random() * (i + 1))
+  ;[array.value[i], array.value[j]] = [array.value[j], array.value[i]]
 }
 
 const audioCtx = new (window.AudioContext || window.webkitAudioContext)()
@@ -44,7 +49,6 @@ function playFrequency(frequency, duration) {
   }, duration)
 }
 
-const interval = 1
 let promise = Promise.resolve()
 
 function bubbleSort(array) {
@@ -72,13 +76,27 @@ function bubbleSort(array) {
       })
     }
   }
+
+  for (let i = 0; i < array.length; i++) {
+    promise = promise.then(() => {
+      chartContainer.value.querySelector(
+        `.chart__bar:nth-of-type(${i + 1})`
+      ).style.backgroundColor = 'green'
+      playFrequency(array[i] * 50, 10)
+      return new Promise((resolve) => {
+        setTimeout(resolve, interval)
+      })
+    })
+  }
 }
 </script>
 
-<style scoped lang="scss">
+<style lang="scss">
+@use '../node_modules/bootstrap/scss/bootstrap';
+
 .chart {
   width: 100%;
-  height: 600px;
+  height: 400px;
   position: relative;
 
   &__bar {
