fecba675fcc801c1c0113713574a9899d5055e68
Sukhvinder Singh <sukhvisukh@gmail.com>
2023-03-28 01:26:32 +0530

    Modified formatting

--- src/App.vue
+++ src/App.vue
@@ -19,61 +19,61 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref } from 'vue';
 
-const chartContainer = ref(null)
-const elementLength = 100
-const interval = 10
+const chartContainer = ref(null);
+const elementLength = 100;
+const interval = 10;
 
-const array = ref([])
+const array = ref([]);
 
 for (let i = 0; i < elementLength; i++) {
-  array.value.push(i + 1)
+  array.value.push(i + 1);
 }
 
 for (let i = array.value.length - 1; i > 0; i--) {
-  const j = Math.floor(Math.random() * (i + 1))
-  ;[array.value[i], array.value[j]] = [array.value[j], array.value[i]]
+  const j = Math.floor(Math.random() * (i + 1));
+  [array.value[i], array.value[j]] = [array.value[j], array.value[i]];
 }
 
-const audioCtx = new (window.AudioContext || window.webkitAudioContext)()
+const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 
 function playFrequency(frequency, duration) {
-  const oscillator = audioCtx.createOscillator()
-  oscillator.type = 'sine'
-  oscillator.frequency.value = frequency // value in hertz
-  oscillator.connect(audioCtx.destination)
-  oscillator.start()
+  const oscillator = audioCtx.createOscillator();
+  oscillator.type = 'sine';
+  oscillator.frequency.value = frequency; // value in hertz
+  oscillator.connect(audioCtx.destination);
+  oscillator.start();
   setTimeout(function () {
-    oscillator.stop()
-  }, duration)
+    oscillator.stop();
+  }, duration);
 }
 
-let promise = Promise.resolve()
+let promise = Promise.resolve();
 
 function bubbleSort(array) {
   for (let i = 0; i <= array.length - 1; i++) {
     for (let j = 0; j < array.length - i - 1; j++) {
       promise = promise.then(() => {
         if (array[j] > array[j + 1]) {
-          let temp = array[j]
-          array[j] = array[j + 1]
-          array[j + 1] = temp
-          playFrequency(array[j] * 50, 10)
+          let temp = array[j];
+          array[j] = array[j + 1];
+          array[j + 1] = temp;
+          playFrequency(array[j] * 50, 10);
         }
 
         chartContainer.value.querySelector(
           `.chart__bar:nth-of-type(${j + 2})`
-        ).style.backgroundColor = 'red'
+        ).style.backgroundColor = 'red';
 
         chartContainer.value.querySelector(
           `.chart__bar:nth-of-type(${j + 1})`
-        ).style.backgroundColor = 'blue'
+        ).style.backgroundColor = 'blue';
 
         return new Promise((resolve) => {
-          setTimeout(resolve, interval)
-        })
-      })
+          setTimeout(resolve, interval);
+        });
+      });
     }
   }
 
@@ -81,12 +81,12 @@ function bubbleSort(array) {
     promise = promise.then(() => {
       chartContainer.value.querySelector(
         `.chart__bar:nth-of-type(${i + 1})`
-      ).style.backgroundColor = 'green'
-      playFrequency(array[i] * 50, 10)
+      ).style.backgroundColor = 'green';
+      playFrequency(array[i] * 50, 10);
       return new Promise((resolve) => {
-        setTimeout(resolve, interval)
-      })
-    })
+        setTimeout(resolve, interval);
+      });
+    });
   }
 }
 </script>
