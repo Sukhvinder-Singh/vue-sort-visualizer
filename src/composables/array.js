7c1a223856d9779deec92d282c7c6176a9f90e2e
Sukhvinder Singh <sukhvisukh@gmail.com>
2023-05-02 00:44:36 +0530

    Added basic documentation

--- src/composables/array.js
+++ src/composables/array.js
@@ -1,7 +1,9 @@
 /**
  *
- * @param {Array} array
- * @param {number} length
+ * Fill an array with values starting from 0 to any length in ascending order
+ *
+ * @param {Array} array - Array to fill with values from 0 to [length] in increasing order
+ * @param {number} length - Length of array
  */
 export const fillArray = (array, length) => {
   for (let i = 0; i < length; i++) {
@@ -11,7 +13,9 @@ export const fillArray = (array, length) => {
 
 /**
  *
- * @param {Array} array
+ * Shuffle elements inside an array
+ *
+ * @param {Array} array - Array whose elements are to be shuffled
  */
 export const shuffleArray = (array) => {
   for (let i = array.length - 1; i > 0; i--) {
